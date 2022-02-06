4bac2193c50f1373d94006a8e3429b23c68ecdae
Adis Tucakovic <57579360+MinyMeep1@users.noreply.github.com>
2022-02-06 13:11:12 -0500

    test
    test

---
+++ src/cs1302/game/MinesweeperDriver.java
@@ -0,0 +1,15 @@
+package cs1302.game;
+
+
+public class MinesweeperDriver {
+    public static void main(String[] args) {
+
+        Scanner stdIn = new Scanner(System.in);
+        private final Scanner stdin;
+
+
+
+
+    }
+
+}
---
+++ src/cs1302/game/MinesweeperGame.java
@@ -0,0 +1,35 @@
+package cs1302.game;
+
+public class MinesweeperGame {
+
+    public void printWelcome() {
+        System.out.println("        _");
+        System.out.println("  /\\/\\ (F)_ __   ___  _____      _____  ___ _ __   ___ _ __");
+        System.out.println(" /    \\| | '_ \\ / _ \\/ __\\ \\ /\\ / / _ \\/ _ \\ '_ \\ / _ \\ '__|");
+        System.out.println("/ /\\/\\ \\ | | | |  __/\\__ \\\\ V  V /  __/  __/ |_) |  __/ |");
+        System.out.println("\\/    \\/_|_| |_|\\___||___/ \\_/\\_/ \\___|\\___| .__/ \\___|_|");
+        System.out.println("                             ALPHA EDITION |_| v2022.sp");
+    }
+
+
+    public void printMineField(int[][] mineField) {
+
+    }
+
+    public void promptUser(){
+
+    }
+
+    public boolean isWon(){
+
+    }
+
+    public void printWin(){
+
+    }
+
+    public void play(){
+
+    }
+
+}
